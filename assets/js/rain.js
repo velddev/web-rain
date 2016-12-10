1f499bde78b819c57997d3dbded2d2a872837b7e
M. Veldsink <velddev@gmail.com>
2016-12-10 17:18:58 +0100

    Add: Alpha and gradients

--- assets/js/rain.js
+++ assets/js/rain.js
@@ -1,16 +1,16 @@
 // Only compatible with ES 6.0+
-
 var canvasWindow = document.getElementById("rain");
 var canvasContext = canvasWindow.getContext("2d");
+canvasContext.globalAlpha = 0;
 
 // Color class
-class Color
-{
-   constructor(r, g, b)
+class Color {
+   constructor(r, g, b, a)
    {
      this.r = r;
      this.g = g;
      this.b = b;
+     this.a = a;
    }
 
    Lerp(c, t)
@@ -18,8 +18,29 @@ class Color
       return new Color(
         this.r + t * (c.r - this.r),
         this.g + t * (c.g - this.g),
-        this.b + t * (c.b - this.b)
+        this.b + t * (c.b - this.b),
+        this.z + t * (c.z - this.z)
       );
+   }
+
+   ReturnFillStyle(z)
+   {
+     canvasContext.fillStyle =
+     "rgba(" +
+     rainColor.r + "," +
+     rainColor.g + "," +
+     rainColor.b + "," +
+     (z * 255) + ")";
+   }
+
+   SetFillStyle(z)
+   {
+     canvasContext.fillStyle =
+     "rgba(" +
+     rainColor.r + "," +
+     rainColor.g + "," +
+     rainColor.b + "," +
+     (0.1 + z) + ")";
    }
 
    ToHex()
@@ -27,13 +48,13 @@ class Color
       return "#" +
       ("0" + parseInt(this.r,10).toString(16)).slice(-2) +
       ("0" + parseInt(this.g,10).toString(16)).slice(-2) +
-      ("0" + parseInt(this.b,10).toString(16)).slice(-2);
+      ("0" + parseInt(this.b,10).toString(16)).slice(-2) +
+      ("0" + parseInt(this.a,10).toString(16)).slice(-2);
    }
 }
 
 // Raindrop class
-class Raindrop
-{
+class Raindrop {
     constructor(x, y, z)
     {
       this.x = x;
@@ -46,35 +67,49 @@ class Raindrop
     Update()
     {
       this.y += this.yvel * this.z;
+      this.yvel *= 1 + this.z / 100;
 
       if(this.y > window.innerHeight)
       {
         this.y = -10;
+        this.yvel = 10;
+        this.x = Random(0, window.innerWidth);
       }
     }
 
     Draw()
     {
-      canvasContext.fillStyle = rainColor.Lerp(bgColor, this.z).ToHex();
-      canvasContext.fillRect(this.x, this.y, 10 * this.z, 40 * this.z);
+      var grd = canvasContext.createLinearGradient
+      (this.x, this.y, this.x + 2 * this.z, this.y + 4 * this.yvel * this.z);
+
+      grd.addColorStop(0, 'rgba(120,240,255,0)');
+      grd.addColorStop(1, 'rgba(100,200,255,0.6)');
+
+      canvasContext.fillStyle = grd;
+
+      canvasContext.fillRect(this.x, this.y,2 * this.z,4 * this.yvel * this.z);
     }
 }
 
-var bgColor = new Color(0, 0, 0);
-var rainColor = new Color(125,249,255);
+var canUpdate = true;
+
+var rainColor = new Color(50,150,255,1);
+
+var angle = 1;
 
 canvasContext.canvas.width  = window.innerWidth;
 canvasContext.canvas.height = window.innerHeight;
 
 var drops = [];
-var dropAmount = 2500;
+var dropAmount = 1250;
 
+// Initialize all
 for(var i = 0; i < dropAmount; i++)
 {
   drops.push(
     new Raindrop(
       Random(0, canvasWindow.width),
-      Random(-100, -450),
+      Random(0, canvasWindow.height),
       i / dropAmount
     )
   );
@@ -84,14 +119,17 @@ setInterval(Tick, 1);
 
 function Tick()
 {
-  console.log("ping");
-  canvasContext.clearRect(0, 0, canvasWindow.width, canvasWindow.height);
-  canvasContext.fillStyle = bgColor.ToHex();
-  canvasContext.fillRect(0, 0, window.innerWidth, window.innerHeight);
-  for(var i = 0; i < dropAmount; i++)
+  if(canUpdate)
   {
-    drops[i].Update();
-    drops[i].Draw();
+    canvasContext.canvas.width  = window.innerWidth;
+    canvasContext.canvas.height = window.innerHeight;
+
+    canvasContext.clearRect(0, 0, canvasWindow.width, canvasWindow.height);
+    for(var i = 0; i < dropAmount; i++)
+    {
+      drops[i].Update();
+      drops[i].Draw();
+    }
   }
 }
 
